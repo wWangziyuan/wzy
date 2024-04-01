f8ed5606395cb8cac884e66c5931c3340e95873c
scrod <xueyuqi@bytedance.com>
2024-04-01 16:18:19 +0800

    chore : 添加基本配置项

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Input} from '@douyinfe/semi-ui';
+import {Title} from '@douyinfe/semi-ui/lib/es/skeleton/item';
+import Text from '@douyinfe/semi-ui/lib/es/typography/text';
+import {useState} from 'react';
 
 function App() {
+const [accountName,setAccountName] = useState('请输入账号')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <Title >欢迎登录教师点名系统</Title>
+          <Input value ={accountName} onChange={(changeValue)=>{setAccountName(changeValue); console.log(accountName)}}></Input>,
+          <Input value={'请输入密码'}></Input>
+          <Button>登录</Button>
+          <Text>如果还没有账号请点击这里注册</Text>
+      </>
   );
 }
 
