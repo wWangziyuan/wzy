bb405ab4f8172f61764210fd237194baf6615f28
scrod <xueyuqi@bytedance.com>
2024-04-02 11:23:48 +0800

    feat : 完成输入框

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import Text from '@douyinfe/semi-ui/lib/es/typography/text';
 import { useState } from 'react';
 
 function App() {
-  const [accountName, setAccountName] = useState('请输入账号');
+  const [accountName, setAccountName] = useState();
+  const [password, setPassword] = useState();
   return (
     <>
       <Title>欢迎登录教师点名系统</Title>
       <Input
         value={accountName}
+        placeholder={'请输入账号'}
         onChange={changeValue => {
           setAccountName(changeValue);
           console.log(accountName);
         }}
       />
-      <Input value={'请输入密码'} />
+      <Input
+        value={password}
+        placeholder={'请输入密码'}
+        onChange={changeValue => {
+          setPassword(changeValue);
+          console.log(setPassword);
+        }}
+      />
       <Button>登录</Button>
       <Text>如果还没有账号请点击这里注册</Text>
       <button>hi</button>
