55d3e8533899b5502edc1842faad6d5a15c925a0
scrod <xueyuqi@bytedance.com>
2024-04-02 11:59:38 +0800

    feat ： 完成登录功能

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import { Button, Input } from '@douyinfe/semi-ui';
 import { Title } from '@douyinfe/semi-ui/lib/es/skeleton/item';
 import Text from '@douyinfe/semi-ui/lib/es/typography/text';
 import { useState } from 'react';
-
 function App() {
   const [accountName, setAccountName] = useState();
   const [password, setPassword] = useState();
+  const login = () => {
+    new Promise((resolve, reject) => {
+      let isSuccessful = Math.random() >= 0.5; // 随机成功或失败
+      if (isSuccessful) {
+        resolve({ success: true }); // 成功，调用resolve
+      } else {
+        reject({ msg: '密码错误', code: 1001 });
+      }
+    })
+      .then(() => {
+        console.log('跳转新页面');
+      })
+      .catch(error => {
+        console.log('错误处理：', error);
+      });
+  };
+
   return (
     <>
       <Title>欢迎登录教师点名系统</Title>
@@ -15,7 +31,6 @@ function App() {
         placeholder={'请输入账号'}
         onChange={changeValue => {
           setAccountName(changeValue);
-          console.log(accountName);
         }}
       />
       <Input
@@ -23,12 +38,16 @@ function App() {
         placeholder={'请输入密码'}
         onChange={changeValue => {
           setPassword(changeValue);
-          console.log(setPassword);
         }}
       />
-      <Button>登录</Button>
+      <Button
+        onClick={() => {
+          login();
+        }}
+      >
+        登录
+      </Button>
       <Text>如果还没有账号请点击这里注册</Text>
-      <button>hi</button>
     </>
   );
 }
