790a50b6ab1c4a78c350e2a0f6f039854be806f6
yizhi <ziyuanWang@moonshotacademy.cn>
2024-05-30 16:03:27 +0800

    123

---
+++ src/pages/course/CourseDemo.js
@@ -0,0 +1,17 @@
+import useSWR from "swr";
+import axios from "axios";
+import { Button, Table, Spin, SideSheet } from "@douyinfe/semi-ui";
+import { useState } from 'react';
+import { AttendenceModal, statusToString } from '@/pages/AttendenceModal';
+
+export const CourseDemo = () => {
+    const [visible, setVisible] = useState(false);
+    const { data, isLoading, mutate } = useSWR(
+        'http://localhost:3000/attences',
+        url => axios.get(url).then(res => res.data),
+    );
+
+    const change = () => {
+        
+    }
+}
